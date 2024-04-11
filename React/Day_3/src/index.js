a91bd9de8dd40270adda473bf5aa77e010dbc459
Aashutosh Rajendra Pawar <aashutoshpawar123@gmail.com>
2024-04-11 14:14:47 +0530

    Delete React directory

--- React/Day_3/src/index.js
+++
@@ -1,8 +0,0 @@
-import react from 'react';
-import  ReactDOM  from 'react-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.header(
-  <h1>
-    hello from react
-  </h1>
-)
