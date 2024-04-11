930828c08e54933b74069f2de17156a7d16531c2
Aashutosh Rajendra Pawar <aashutoshpawar123@gmail.com>
2024-04-11 14:14:26 +0530

    Add files via upload

---
+++ React/Day_3/src/index.js
@@ -0,0 +1,8 @@
+import react from 'react';
+import  ReactDOM  from 'react-dom';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.header(
+  <h1>
+    hello from react
+  </h1>
+)
